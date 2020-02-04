a71d967dad0d8eb017b95be3111a15c91ea4a980
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-04 12:56:57 -0500

    refactor(fuelData.js and getFuelRequirements.js): move I/O to it's own file and refactor for loop to make it more declarative

--- day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
+++ day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
@@ -1,35 +1,19 @@
-const fs = require('fs');
-const path = require('path')
-const { sum } = require('ramda');
-const text = path.join(__dirname, 'module-mass.txt')
-const massArray = fs.readFileSync(path.join(text)).toString('utf-8').split("\n");
-//individually calculate the fuel needed for each mass of each module
-//to calculate the individual fuel requirements divide the mass by 3, then round down, and subtract 2
-//then sum together all the fuel requirements 
-//each fuel also requires it's own fuel
+const { sum, map } = require('ramda');
+const massArray = require('./fuelData');
 
-const findfuel = num => {
+const findFuel = num => {
   return Math.floor(num / 3) - 2;
 };
 
-const totalfuel = array => {
+const currentFuel = (currentMass) => {
+  const fuel = findFuel(currentMass);
+  return (fuel <= 0 ? 0 : fuel + currentFuel(fuel));
+}
 
-  let fuelArray = [];
-
-  for (let index = 0; index < array.length; index++) {
-    let currentMass = (parseInt(array[index]));
-    let totalfuel = 0;
-
-    while (findfuel(currentMass) > 0) {
-      totalfuel += findfuel(currentMass);
-      currentMass = findfuel(currentMass);
-    }
-    fuelArray.push(totalfuel);
-  }
-
-  return sum(fuelArray);
+const totalFuel = array => {
+  return sum(map(currentFuel, array));
 };
 
-console.log(totalfuel(massArray));
+console.log(totalFuel(massArray));
 
-module.exports = totalfuel;
+module.exports = totalFuel;
--- day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
+++ day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
@@ -1,16 +1,16 @@
 const { expect } = require('chai');
-const totalfuel = require('../getFuelRequirements');
+const totalFuel = require('../getFuelRequirements');
 
-describe('totalfuel', () => {
+describe('totalFuel', () => {
   it('outputs the correct amount of fuel when given a single input', () => {
-    expect(totalfuel([100756])).to.equal(50346);
+    expect(totalFuel([100756])).to.equal(50346);
   })
 
   it('outputs the correct amount of fuel when given multiple inputs', () => {
-    expect(totalfuel([24, 37, 1009, 2056])).to.equal(1512);
+    expect(totalFuel([24, 37, 1009, 2056])).to.equal(1512);
   })
 
   it('treats negative fuel as zero', () => {
-    expect(totalfuel[0]).to.equal(totalfuel[-6]);
+    expect(totalFuel([0])).to.equal(totalFuel([-6]));
   })
 });
