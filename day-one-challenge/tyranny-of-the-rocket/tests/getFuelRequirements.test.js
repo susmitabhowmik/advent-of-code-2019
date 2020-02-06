12151906f7999c2918935e0d8c83671e1f2a86bb
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-06 14:53:45 -0500

    refactor(getFuelRequirements.js and getFuelRequirements.test.js): refactor to add more descriptive names

--- day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
+++ day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
@@ -1,28 +1,28 @@
 const { expect } = require('chai');
-const { totalFuel, findFuel, currentFuel } = require('../getFuelRequirements');
+const { totalFuelForAllModulesOfMass, calculateFuel, fuelOfCurrentModuleOfMass } = require('../getFuelRequirements');
 
-describe('totalFuel', () => {
+describe('totalFuelForAllModulesOfMass', () => {
   it('outputs the correct amount of fuel when given a single input', () => {
-    expect(totalFuel([100756])).to.equal(50346);
+    expect(totalFuelForAllModulesOfMass([100756])).to.equal(50346);
   })
 
   it('outputs the correct amount of fuel when given multiple inputs', () => {
-    expect(totalFuel([24, 37, 1009, 2056])).to.equal(1512);
+    expect(totalFuelForAllModulesOfMass([24, 37, 1009, 2056])).to.equal(1512);
   })
 
   it('treats negative fuel as zero', () => {
-    expect(totalFuel([0])).to.equal(totalFuel([-6]));
+    expect(totalFuelForAllModulesOfMass([0])).to.equal(totalFuelForAllModulesOfMass([-6]));
   })
 });
 
-describe('findFuel', () => {
+describe('calculateFuel', () => {
   it('takes a number, divides the number by 3, rounds down, and then subtract two', () => {
-    expect(findFuel(100)).to.equal(31);
+    expect(calculateFuel(100)).to.equal(31);
   })
 })
 
-describe('currentFuel', () => {
+describe('fuelOfCurrentModuleOfMass', () => {
   it('treats fuel as though it has mass and performs math on fuel until fuel is less than or equal to 0, then returns sum of all fuel required for mass module', () => {
-    expect(currentFuel(1969)).to.equal(966);
+    expect(fuelOfCurrentModuleOfMass(1969)).to.equal(966);
   })
 })
