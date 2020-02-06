fd51f96084fc7b5eb603586a9832310ab2baad63
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-06 16:33:42 -0500

    feat(getFuelRequirements.js): add type error handling

--- day-one-challenge/tyranny-of-the-rocket/fuelData.js
+++ day-one-challenge/tyranny-of-the-rocket/fuelData.js
@@ -2,7 +2,7 @@ const { map } = require('ramda');
 const fs = require('fs');
 const path = require('path');
 const text = path.join(__dirname, 'module-mass.txt');
-const numAsStringArray = fs.readFileSync(path.join(text)).toString('utf-8').split("\n");
-const massArray = (map(parseInt, numAsStringArray));
+const numAsStringArray = fs.readFileSync(text).toString('utf-8').split("\n");
+const moduleMassArray = (map(parseInt, numAsStringArray));
 
-module.exports = massArray;
+module.exports = moduleMassArray;
