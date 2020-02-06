ee4640693dccf8e3cd7dbdb9fdaeddedc0fc73ea
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-06 13:35:28 -0500

    refactor(getFuelRequirements.js): refactor to utilize tail call optimization

--- day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
+++ day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
@@ -5,10 +5,9 @@ const findFuel = num => {
   return Math.floor(num / 3) - 2;
 };
 
-const currentFuel = (currentMass) => {
-  const fuel = findFuel(currentMass);
-  return (fuel <= 0 ? 0 : fuel + currentFuel(fuel));
-}
+const currentFuel = (currentMass) =>
+  findFuel(currentMass) <= 0 ? 0 : findFuel(currentMass) + currentFuel(findFuel(currentMass))
+
 
 const totalFuel = array => {
   return sum(map(currentFuel, array));
