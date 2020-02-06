eb209ac2b1483fd1665d45720b7bad5b69c36f98
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-06 13:03:49 -0500

    test(getFuelRequirements.test.js): add tests for findFuel and currentFuel

--- day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
+++ day-one-challenge/tyranny-of-the-rocket/getFuelRequirements.js
@@ -5,10 +5,9 @@ const findFuel = num => {
   return Math.floor(num / 3) - 2;
 };
 
-const currentFuel = (currentMass) => {
-  const fuel = findFuel(currentMass);
-  return (fuel <= 0 ? 0 : fuel + currentFuel(fuel));
-}
+const currentFuel = (currentMass) =>
+  findFuel(currentMass) <= 0 ? 0 : findFuel(currentMass) + currentFuel(findFuel(currentMass))
+
 
 const totalFuel = array => {
   return sum(map(currentFuel, array));
@@ -16,4 +15,4 @@ const totalFuel = array => {
 
 console.log(totalFuel(massArray));
 
-module.exports = totalFuel;
+module.exports = { totalFuel, currentFuel, findFuel }
--- day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
+++ day-one-challenge/tyranny-of-the-rocket/tests/getFuelRequirements.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const totalFuel = require('../getFuelRequirements');
+const { totalFuel, findFuel, currentFuel } = require('../getFuelRequirements');
 
 describe('totalFuel', () => {
   it('outputs the correct amount of fuel when given a single input', () => {
@@ -14,3 +14,15 @@ describe('totalFuel', () => {
     expect(totalFuel([0])).to.equal(totalFuel([-6]));
   })
 });
+
+describe('findFuel', () => {
+  it('takes a number, divides the number by 3, rounds down, and then subtract two', () => {
+    expect(findFuel(100)).to.equal(31);
+  })
+})
+
+describe('currentFuel', () => {
+  it('treats fuel as though it has mass and performs math on fuel until fuel is less than or equal to 0, then returns sum of all fuel required for mass module', () => {
+    expect(currentFuel(1969)).to.equal(966);
+  })
+})
