12443b7526708cd46a9f4ccf6b3958d450385988
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-10 11:05:54 -0500

    feat(index.js): add driver for solutions to AOC

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const { findPasswords } = require('./day-four-challenge/secure-container/getPasswords')
+const { totalFuelForAllModulesOfMass } = require('./day-one-challenge/tyranny-of-the-rocket/getFuelRequirements');
+const moduleMassArray = require('./day-one-challenge/tyranny-of-the-rocket/fuelData');
 
+console.log(`Day one: ${totalFuelForAllModulesOfMass(moduleMassArray)}`);
 console.log(`Day four: ${findPasswords()}`);
+
+
+
