ee881cd3fe31fb590002f57535e626eeeb823f42
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-12 15:15:40 -0500

    refactor(getPasswords.js): move minimum length requirements to its own function

--- day-four-challenge/secure-container/getPasswords.js
+++ day-four-challenge/secure-container/getPasswords.js
@@ -1,27 +1,30 @@
-const { map, sort, not, uniq, equals, unfold, reduceWhile, add } = require('ramda');
+const { map, sort, not, uniq, equals, unfold } = require('ramda');
 
 const MIN = 359282;
 const MAX = 820401;
+const PASSWORD_MIN_LENGTH = 6;
 
 const diff = (a, b) => { return a - b };
 
 const sortedArray = array => (
-  equals(sort(diff, array), array) ? true : false
+  equals(sort(diff, array), array)
 );
 
-
 const repeatingArray = (array) => (not(equals(uniq(array), array)))
+
+const minLengthRequirement = (array) => array.length === PASSWORD_MIN_LENGTH;
 
 const checkDigits = (num) => {
   const numStringArray = num.toString().split('');
   const numArray = map(parseInt, numStringArray);
 
-  return numArray.length === 6 && sortedArray(numArray) && repeatingArray(numArray) ? true : false
+  return minLengthRequirement(numArray) && sortedArray(numArray) && repeatingArray(numArray)
 }
 
-function findPasswords(num, max) {
+function findPasswords(num = MIN, max = MAX) {
   const f = num => num > max ? false : [num, num + 1];
   const array = unfold(f, num);
+  //console.log(array);
   return array.filter(num => checkDigits(num)).length;
 }
 
