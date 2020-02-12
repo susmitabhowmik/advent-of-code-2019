d0ee99b5784105cb647b6492e192f9f89752068b
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-12 13:29:59 -0500

    refactor(getPasswords.js): refactor findPasswords to be more functional

--- day-four-challenge/secure-container/getPasswords.js
+++ day-four-challenge/secure-container/getPasswords.js
@@ -1,4 +1,7 @@
-const { map, sort, not, uniq, equals } = require('ramda');
+const { map, sort, not, uniq, equals, unfold, reduceWhile, add } = require('ramda');
+
+const MIN = 359282;
+const MAX = 820401;
 
 const diff = (a, b) => { return a - b };
 
@@ -6,8 +9,8 @@ const sortedArray = array => (
   equals(sort(diff, array), array) ? true : false
 );
 
-const repeatingArray = (array) => (not(equals(uniq(array), array)))
 
+const repeatingArray = (array) => (not(equals(uniq(array), array)))
 
 const checkDigits = (num) => {
   const numStringArray = num.toString().split('');
@@ -16,14 +19,12 @@ const checkDigits = (num) => {
   return numArray.length === 6 && sortedArray(numArray) && repeatingArray(numArray) ? true : false
 }
 
-const findPasswords = (min, max) => {
-  let counter = 0;
-  for (let i = min; i <= max; i++) {
-    if (checkDigits(i) === true) {
-      counter++;
-    }
-  }
-  return counter;
+function findPasswords(num, max) {
+  const f = num => num > max ? false : [num, num + 1];
+  const array = unfold(f, num);
+  return array.filter(num => checkDigits(num)).length;
 }
+
+console.log(findPasswords(359282, 820401));
 
 module.exports = { findPasswords, checkDigits, repeatingArray, sortedArray }
