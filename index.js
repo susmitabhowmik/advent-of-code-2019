339efc241058690fab07d5ad876cfbf103ec9729
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-13 15:04:18 -0500

    refactor(index.js): remove max and min constants because they are default params

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const { findPasswords } = require('./day-four-challenge/secure-container/getPasswords')
-const MIN = 359282;
-const MAX = 820401;
 
-console.log(`Day four: ${findPasswords(MIN, MAX)}`);
+console.log(`Day four: ${findPasswords()}`);
