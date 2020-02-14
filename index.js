24f6388842749ad7ce1eccb0e1c60bdc37d985e8
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-14 09:34:37 -0500

    chore(package.json and index.js): incorporate changes from day 4 merge

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { findPasswords } = require('./day-four-challenge/secure-container/getPasswords')
-
-console.log(`Day four: ${findPasswords()}`);
 const { totalFuelForAllModulesOfMass } = require('./day-one-challenge/tyranny-of-the-rocket/getFuelRequirements');
 const moduleMassArray = require('./day-one-challenge/tyranny-of-the-rocket/fuelData');
 
-console.log(`day one: ${totalFuelForAllModulesOfMass(moduleMassArray)}`);
+console.log(`Day one: ${totalFuelForAllModulesOfMass(moduleMassArray)}`);
+console.log(`Day four: ${findPasswords()}`);
+
+
+
