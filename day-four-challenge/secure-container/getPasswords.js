e4f230d4a00512c9b2a8ac21cf5d47222c99b912
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-17 13:56:34 -0500

    feat(getPasswords.js): add function which removes all elements which occur more than twice in a given array

--- day-four-challenge/secure-container/getPasswords.js
+++ day-four-challenge/secure-container/getPasswords.js
@@ -14,6 +14,18 @@ const hasAtLeastTwoRepeatingDigits = (array) => (not(equals(uniq(array), array))
 
 const isASixDigitNumber = (array) => array.length === PASSWORD_MIN_LENGTH;
 
+const removeDigitsWhichRepeatMoreThanTwice = (array) => {
+  let num;
+  let newArray = array;
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] === newArray[i + 1] && newArray[i] === newArray[i + 2]) num = newArray[i];
+    while (newArray[i] === num) {
+      newArray = newArray.slice(0, i).concat(newArray.slice(i + 1))
+    }
+  }
+  return newArray;
+}
+
 const testPassWordRequirements = (array) = allPass([isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits]);
 
 const createNumArray = (num) => {
@@ -21,14 +33,16 @@ const createNumArray = (num) => {
   return map(parseInt, numStringArray);
 }
 
+
 const meetsPasswordRequirements = (num) => {
   return pipe(createNumArray, testPassWordRequirements)(num);
 }
 
+//takes in a range of numbers and unfolds them into an array of numbers within the range, checks to see if each password matches the requirement and then returns length of array
 function findPasswords(num = MIN, max = MAX) {
   const f = num => num > max ? false : [num, num + 1];
   const array = unfold(f, num);
   return array.filter(num => meetsPasswordRequirements(num)).length;
 }
 
-module.exports = { findPasswords, meetsPasswordRequirements, isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits };
+module.exports = { findPasswords, meetsPasswordRequirements, isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits, removeDigitsWhichRepeatMoreThanTwice };
