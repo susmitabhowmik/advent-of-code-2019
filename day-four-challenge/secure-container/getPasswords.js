e8a0ca38cf201cbfac8a7397f75b1b7c6b159a79
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-18 13:01:24 -0500

    style(getPasswords.js): add comments to explain what each function does

--- day-four-challenge/secure-container/getPasswords.js
+++ day-four-challenge/secure-container/getPasswords.js
@@ -2,19 +2,24 @@ const { map, sort, not, uniq, equals, unfold, allPass, pipe, join } = require('r
 
 const MIN = 359282;
 const MAX = 820401;
-const PASSWORD_MIN_LENGTH = 6;
+const PASSWORD_LENGTH = 6;
 
+//returns difference of two numbers
 const diff = (a, b) => { return a - b };
 
+//returns true or false based on if digits increase or stay the same from left to right
 const digitsIncreaseOrStayTheSameFromLeftToRight = array => (
   equals(sort(diff, array), array)
 );
 
+//returns true or false based on if an array contains at least two repeating digits
 const hasAtLeastTwoRepeatingDigits = (array) => (not(equals(uniq(array), array)));
 
-const isASixDigitNumber = (array) => array.length === PASSWORD_MIN_LENGTH;
+//returns true or false based on if an array has a length of 6 
+const isASixDigitNumber = (array) => array.length === PASSWORD_LENGTH;
 
-const removeDigitsWhichRepeatMoreThanTwice = (array) => {
+//determines if all repeating digits are exactly two digits long
+const areAllRepeatingDigitsExactlyTwoDigitsLong = (array) => {
   let newNumAsString = join('', array);
   for (let i = 0; i < newNumAsString.length; i++) {
     const regex = new RegExp(`${newNumAsString[i]}{3,}`);
@@ -24,14 +29,16 @@ const removeDigitsWhichRepeatMoreThanTwice = (array) => {
   return hasAtLeastTwoRepeatingDigits(newNum);
 }
 
-const testPassWordRequirements = (array) = allPass([isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits, removeDigitsWhichRepeatMoreThanTwice]);
+//return boolean based on password requirements to test that the number is exactly six digits, digits increase from left to right, and all repeating digits are exactly two digits long
+const testPassWordRequirements = (array) = allPass([isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, areAllRepeatingDigitsExactlyTwoDigitsLong]);
 
+//take in a number and output an array containing all the digits in the number
 const createNumArray = (num) => {
   const numStringArray = num.toString().split('');
   return map(parseInt, numStringArray);
 }
 
-
+//return boolean based on if number passes all password requirements
 const meetsPasswordRequirements = (num) => {
   return pipe(createNumArray, testPassWordRequirements)(num);
 }
@@ -40,8 +47,7 @@ const meetsPasswordRequirements = (num) => {
 function findPasswords(num = MIN, max = MAX) {
   const f = num => num > max ? false : [num, num + 1];
   const array = unfold(f, num);
-  console.log(array.filter(num => meetsPasswordRequirements(num)));
   return array.filter(num => meetsPasswordRequirements(num)).length;
 }
 
-module.exports = { findPasswords, meetsPasswordRequirements, isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits, removeDigitsWhichRepeatMoreThanTwice };
+module.exports = { findPasswords, meetsPasswordRequirements, isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, hasAtLeastTwoRepeatingDigits, areAllRepeatingDigitsExactlyTwoDigitsLong };
