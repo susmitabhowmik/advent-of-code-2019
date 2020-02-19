c73bc203756d9773d7cfe67e3d1268959237e033
Susmita Bhowmik <susmita@heysparkbox.com>
2020-02-19 14:17:16 -0500

    docs(getPasswords.js): update comments to utilize JSDoc

--- day-four-challenge/secure-container/getPasswords.js
+++ day-four-challenge/secure-container/getPasswords.js
@@ -4,49 +4,86 @@ const MIN = 359282;
 const MAX = 820401;
 const PASSWORD_LENGTH = 6;
 
-//returns difference of two numbers
+/**
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number} 
+ */
 const diff = (a, b) => { return a - b };
 
-//returns true or false based on if digits increase or stay the same from left to right
+/** 
+ * @param {Array.Number} array 
+ * @returns {Boolean}
+ */
 const digitsIncreaseOrStayTheSameFromLeftToRight = array => (
   equals(sort(diff, array), array)
 );
 
-//returns true or false based on if an array contains at least two repeating digits
+/** 
+ * @param {Array.Number} array 
+ * @returns {Boolean}
+ */
 const hasAtLeastTwoRepeatingDigits = (array) => (not(equals(uniq(array), array)));
 
-//returns true or false based on if an array has a length of 6 
+/**
+ * @param {Array.Number} array
+ * @returns {Number} 
+ */
 const isASixDigitNumber = (array) => array.length === PASSWORD_LENGTH;
 
+/**
+ * @param {Array.Number} array 
+ * @returns {String}
+ */
 const arrayToString = (array) => join('', array);
 
+/** 
+ * @param {String} string 
+ * @returns {String}
+ */
 const removeDigitsRepeatingMoreThanTwice = (string) => {
   const regex = new RegExp('0{3,}|1{3,}|2{3,}|3{3,}|4{3,}|5{3,}|6{3,}|7{3,}|8{3,}|9{3,}', 'g');
-  return replace(regex, '', string)
+  return replace(regex, '', string) //'7777884 => '884'
 }
 
-//determines if all repeating digits are exactly two digits long
+/**
+ * @param {Array.Number} array 
+ * @returns {Boolean}
+ */
 const areAllRepeatingDigitsExactlyTwoDigitsLong = (array) => {
   const newNumAsString = removeDigitsRepeatingMoreThanTwice(arrayToString(array))
   const newNum = map(parseInt, newNumAsString);
   return hasAtLeastTwoRepeatingDigits(newNum);
 }
 
-//return boolean based on password requirements to test that the number is exactly six digits, digits increase from left to right, and all repeating digits are exactly two digits long
+/**
+ * @param {Array.Number} array
+ * @returns {Boolean}
+ */
 const testPassWordRequirements = (array) = allPass([isASixDigitNumber, digitsIncreaseOrStayTheSameFromLeftToRight, areAllRepeatingDigitsExactlyTwoDigitsLong]);
 
-//take in a number and output an array containing all the digits in the number
+/**
+ * @param {Number} num 
+ * @returns {Array.Number}
+ */
 const createNumArray = (num) => {
   const numStringArray = num.toString().split('');
   return map(parseInt, numStringArray);
 }
 
-//return boolean based on if number passes all password requirements
+/**
+ * @param {Number} num 
+ * @returns {Boolean}
+ */
 const meetsPasswordRequirements = (num) => {
   return pipe(createNumArray, testPassWordRequirements)(num);
 }
 
-//takes in a range of numbers and unfolds them into an array of numbers within the range, checks to see if each password matches the requirement and then returns length of array
+/**
+ * @param {Number} num 
+ * @param {Number} max 
+ * @returns {Boolean}
+ */
 function findPasswords(num = MIN, max = MAX) {
   const f = num => num > max ? false : [num, num + 1];
   const array = unfold(f, num);
