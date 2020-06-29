5a7598868a85122672b288060c71302cfcc4a04a
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-06-28 21:41:29 -0400

    feat: add Datadog monitoring

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const { findPasswords } = require('./day-four-challenge/secure-container/getPasswords')
-const { totalFuelForAllModulesOfMass } = require('./day-one-challenge/tyranny-of-the-rocket/getFuelRequirements');
-const moduleMassArray = require('./day-one-challenge/tyranny-of-the-rocket/fuelData');
+var StatsD = require("node-dogstatsd").StatsD;
+var dogstatsd = new StatsD();
+
+//Increment a counter.
+dogstatsd.increment("page.views");
+
+const {
+  findPasswords,
+} = require("./day-four-challenge/secure-container/getPasswords");
+const {
+  totalFuelForAllModulesOfMass,
+} = require("./day-one-challenge/tyranny-of-the-rocket/getFuelRequirements");
+const moduleMassArray = require("./day-one-challenge/tyranny-of-the-rocket/fuelData");
 
 console.log(`Day one: ${totalFuelForAllModulesOfMass(moduleMassArray)}`);
 console.log(`Day four: ${findPasswords()}`);
-
-
-
